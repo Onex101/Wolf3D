2dc6505b4f8f5d23c1edb3defd79ff9d671db419
Samantha HILLEBRAND <shillebr@c1r3s3.wethinkcode.co.za>
2018-07-25 13:51:20 +0200

    Editing code

--- raycast.c
+++ raycast.c
@@ -6,48 +6,23 @@
 /*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 06:40:16 by shillebr          #+#    #+#             */
-/*   Updated: 2018/07/25 06:42:27 by shillebr         ###   ########.fr       */
+/*   Updated: 2018/07/25 13:51:02 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_horizontal_check()
+int		ft_horizontal_check(t_player *p)
 {
 	int		ya;
 	int		xa;
-
-	//Find first grid intersection
-	if (theta > -90 && < 90)
-		ay = (player_y/64) * (64) - 1;
-	else if (theta < -90 && > 90)
-		ay = (player_y / 64) * 64 + 64;
-	ay = ay / 64;
-	ax = player_x + (player_y - ay) / tan(60);
-	ax = ax / 64;
-
-	//Find Ya
-	if (theta > -90 && < 90)
-		ya = -64;
-	else if (theta < -90 && > 90)
-		ya = 64;
-	xa = 64 / tan(60); //xa = 36;
-
-	//Coordinates of next intersection
-	cx = ax + xa; //original xa val, not grid position
-	cx = cx / 64;
-	cy = ay + ya;
-	cy = cy / 64;
-
-	//loop through until hit a "wall"
-	//
 	int		inter_x;
 	int		inter_y;
 
 	if (theta > -90 && theta < 90)
-		ay = (player_y/64) * (64) - 1;
+		ay = (p->pos.y/64) * (64) - 1;
 	else if (theta < -90 && theta > 90)
-		ay = (player_y / 64) * 64 + 64;
+		ay = (p->pos.y / 64) * 64 + 64;
 	inter_y = ay / 64;
-	ax = player_x + (player_y - ay) / tan(60);
+	ax = p->pos.x + (p->pos.y - ay) / tan(FOV);
 	inter_x = ax / 64;
 
 	//Find Ya
@@ -55,7 +30,7 @@ int		ft_horizontal_check()
 		ya = -64;
 	else if (theta < -90 && > 90)
 		ya = 64;
-	xa = 64 / tan(60); //xa = 36;
+	xa = 64 / tan(FOV); //xa = 36;
 
 	while (grid(inter_y)(inter_x) != 'X')
 	{
@@ -63,7 +38,40 @@ int		ft_horizontal_check()
 		inter_x = ax / 64;
 		ay = ay + ya;
 		inter_y = ay / 64;
-		if (inter_x < 0 || inter_x >= screen_width || inter_y < 0 || inter_y >= screen height);
+		if (inter_x < 0 || inter_x >= WIDTH || inter_y < 0 || inter_y >= HEIGHT);
+			exit (0);
+	}
+}
+
+int		ft_vertical_check(t_player *p)
+{
+	int		ya;
+	int		xa;
+	int		inter_x;
+	int		inter_y;
+
+	if (theta > 0 && theta < 180)
+		ay = (p->pos.y/64) * (64) - 1;
+	else if (theta < 0 && theta > 90)
+		ay = (p->pos.y / 64) * 64 + 64;
+	inter_y = ay / 64;
+	ax = p->pos.x + (p->pos.y - ay) / tan(FOV);
+	inter_x = ax / 64;
+
+	//Find Ya
+	if (theta > -90 && < 90)
+		ya = -64;
+	else if (theta < -90 && > 90)
+		ya = 64;
+	xa = 64 / tan(FOV); //xa = 36;
+
+	while (grid(inter_y)(inter_x) != 'X')
+	{
+		ax = ax + xa;
+		inter_x = ax / 64;
+		ay = ay + ya;
+		inter_y = ay / 64;
+		if (inter_x < 0 || inter_x >= WIDTH || inter_y < 0 || inter_y >= HEIGHT);
 			exit (0);
 	}
 }
