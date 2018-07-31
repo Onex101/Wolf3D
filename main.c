64a5a16c1d1f89dab322857384d79c4d806349b6
Samantha HILLEBRAND <shillebr@c1r3s3.wethinkcode.co.za>
2018-07-31 06:52:16 +0200

    Fixed Merge mistake

--- main.c
+++ main.c
@@ -3,26 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 07:40:00 by xrhoda            #+#    #+#             */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-/*   Updated: 2018/07/30 10:16:40 by xrhoda           ###   ########.fr       */
-=======
-<<<<<<< HEAD
->>>>>>> 3dd25077d057e70c5b5e12a5ff2113bbb10f9e66
-/*   Updated: 2018/07/30 14:34:26 by shillebr         ###   ########.fr       */
-/*   Updated: 2018/07/30 09:42:00 by xrhoda           ###   ########.fr       */
-<<<<<<< HEAD
-=======
-=======
-/*   Updated: 2018/07/30 09:12:24 by shillebr         ###   ########.fr       */
->>>>>>> 379ff54766860855e531f0c31e8c107ab66e28a9
->>>>>>> 0a51f07c89c16a50f2b274168bb522b55a1df80d
->>>>>>> 6f2982132e3ed06c1820ff2cd78715705ce8b3e0
->>>>>>> 3dd25077d057e70c5b5e12a5ff2113bbb10f9e66
+/*   Updated: 2018/07/31 06:51:48 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,27 +26,16 @@ void	init_param(t_param *p, char *str)
 
 int		draw_to_screen(t_param *p)
 {
-	//int		i;
+	int		i;
 
 	mlx_clear_window(p->mlx, p->win);
 	draw_map(p);
-<<<<<<< HEAD
 	ft_putendl("test1");
 	i = ft_rays(p);
 	if (i > 0)
 		ft_putendl("Yay");
 	ft_putendl("test2");
-	// draw_player(); // wrtie draw_player;
-=======
-<<<<<<< HEAD
-	//i = ft_rays(p);
-=======
-	//ft_putendl("test1");
-	//i = ft_rays(p);
-	//ft_putendl("test2");
->>>>>>> 6f2982132e3ed06c1820ff2cd78715705ce8b3e0
 	//draw_player(); // wrtie draw_player;
->>>>>>> 3dd25077d057e70c5b5e12a5ff2113bbb10f9e66
 	return (0);
 }
 
