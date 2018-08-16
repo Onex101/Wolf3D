f22144dab352b8debc0e1cc3df5b7b57eb53404c
Samantha HILLEBRAND <shillebr@c1r3s3.wethinkcode.co.za>
2018-08-16 14:02:24 +0200

    Fixing walls

--- ft_ray_checks.c
+++ ft_ray_checks.c
@@ -6,7 +6,7 @@
 /*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/06 06:57:25 by shillebr          #+#    #+#             */
-/*   Updated: 2018/08/13 08:31:51 by shillebr         ###   ########.fr       */
+/*   Updated: 2018/08/16 13:55:53 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,73 @@
 #include "trig_tables.h"
 #include <stdio.h>
 
+double     ft_db_mod(double a, double b)
+{
+    double  mod;
+    int     dv;
+
+    dv = (int)(a / b);
+    mod = a - ((double)dv * b);
+    return (mod);
+}
+
+int		ft_corner(t_check **h, t_param *par, t_vec2 *a)
+{
+	void	*s1;
+	void	*s2;
+	int		i1;
+	int		i2;
+
+	if ((*h)->xa >= 0)
+		i1 = par->map->max_x * (*h)->arr->y + ((*h)->arr->x - 1);
+	else
+		i1 = par->map->max_x * (*h)->arr->y + ((*h)->arr->x + 1);
+	if ((*h)->ya >= 0)
+		i2 = par->map->max_x * ((*h)->arr->y - 1) + ((*h)->arr->x);
+	else
+		i2 = par->map->max_x * ((*h)->arr->y + 1) + (*h)->arr->x;
+	s1 = vector_get(par->map->ver_vec, i1);
+	s2 = vector_get(par->map->ver_vec, i2);
+	if (ft_db_mod(a->x, par->x_scale) == 0 && ft_db_mod(a->y, par->y_scale) == 0)
+	{
+		if ((((t_vec3 *)((*h)->pos))->z) != 0)
+			return (1);
+		else if (((t_vec3 *)s1)->z != 0 && ((t_vec3 *)s2)->z != 0)
+			return (1);
+	}
+	return (0);
+}
+
+int		ft_wall_check(t_check **h, t_param *par, t_vec2 *a)
+{
+	int		i;
+
+	if (a->x == 0 || a->x == par->map->max_x * (par->x_scale + 1) - 1)
+		return (1);
+	else if (a->y == 0 || a->y == par->map->max_y * (par->y_scale + 1) - 1)
+		return (1);
+	if (a->x < 0 || a->x >= par->map->max_x * (par->x_scale + 1))
+		return (0);
+	else if (a->y < 0 || a->y >= par->map->max_y * (par->y_scale + 1))
+		return (0);
+	(*h)->arr->x = (a->x ) / par->x_scale;
+	(*h)->arr->y = (a->y) / par->y_scale;
+	if (ft_corner(h, par, a))
+		return (1);
+	if ((*h)->arr->x < 0 || (*h)->arr->x > par->map->max_x)
+		return (0);
+	else if ((*h)->arr->y < 0 || (*h)->arr->y > par->map->max_y)
+		return (0);
+	i = par->map->max_x * (*h)->arr->y + (*h)->arr->x;
+	if (!((*h)->pos = vector_get(par->map->ver_vec, i)))
+		return (0);
+	return (2);
+}
+
 t_vec2		*ft_find_wall(t_check **h, t_param *par)
 {
 	t_vec2	*a;
+	int		i;
 
 	if (!(a = ft_vec2_init((*h)->col->x, (*h)->col->y)))
 		return (NULL);
@@ -26,15 +90,12 @@ t_vec2		*ft_find_wall(t_check **h, t_param *par)
 		return (NULL);
 	while ((((t_vec3 *)((*h)->pos))->z) == 0)
 	{
-		if ((*h)->arr->x == 0 || (*h)->arr->x == par->map->max_x || (*h)->arr->y == 0 || (*h)->arr->y == par->map->max_y)
-			break ;
 		a->x = a->x + (*h)->xa;
-		(*h)->arr->x = (a->x ) / par->x_scale;
 		a->y = a->y + (*h)->ya;
-		(*h)->arr->y = (a->y) / par->y_scale;
-		if ((*h)->arr->x < 0 || (*h)->arr->x > par->map->max_x || (*h)->arr->y < 0 || (*h)->arr->y > par->map->max_y)
-			return (NULL);
-		if (!((*h)->pos = vector_get(par->map->ver_vec, (par->map->max_x * (*h)->arr->y + (*h)->arr->x))))
+		i = ft_wall_check(h, par, a);
+		if (i == 1)
+			break ;
+		else if (i == 0)
 			return (NULL);
 	}
 	return (a);
@@ -84,6 +145,22 @@ int		ft_hori_check(t_player *p, t_dist **d, t_param *par)
 	h->arr->x = h->col->x / par->x_scale;
 	if (!((*d)->h_dist = ft_find_wall(&h, par)))
 		return (0);
+	if (h->ya > 0)
+	{
+		(*d)->h_dist->y = (*d)->h_dist->y - 1;//South
+		if (h->xa > 0)
+			(*d)->h_dist->x = (*d)->h_dist->x + 1;
+		else if (h->xa < 0)
+			(*d)->h_dist->x = (*d)->h_dist->x + 1;
+	}
+	else if (h->ya < 0)
+	{
+		(*d)->h_dist->y = (*d)->h_dist->y + 1;//North
+		if (h->xa > 0)
+			(*d)->h_dist->x = (*d)->h_dist->x - 1;
+		else if (h->xa < 0)
+			(*d)->h_dist->x = (*d)->h_dist->x + 1;
+	}
 	(*d)->h_distance = ft_dist_calc(p, (*d)->h_dist);
 	ft_check_free(h);
 	// free(h);
@@ -136,6 +213,22 @@ int		ft_vert_check(t_player *p, t_dist **d, t_param *par)
 	h->arr->x = h->col->x / par->x_scale;
 	if (!((*d)->v_dist = ft_find_wall(&h, par)))
 		return (0);
+	if (h->xa > 0)
+	{
+		(*d)->v_dist->x = (*d)->v_dist->x - 1;//East
+		if (h->ya > 0)
+			(*d)->v_dist->y = (*d)->v_dist->y - 1;
+		else if (h->ya < 0)
+			(*d)->v_dist->y = (*d)->v_dist->y + 1;
+	}
+	else if (h->xa < 0)
+	{
+		(*d)->v_dist->x = (*d)->v_dist->x + 1;//West
+		if (h->ya > 0)
+			(*d)->v_dist->y = (*d)->v_dist->y - 1;
+		else if (h->ya < 0)
+			(*d)->v_dist->y = (*d)->v_dist->y + 1;
+	}
 	(*d)->v_distance = ft_dist_calc(p, (*d)->v_dist);
 	ft_check_free(h);
 	// free(h);
