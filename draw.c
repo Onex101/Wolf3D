060c1186e6aff9e85702cc24cec1280d61dbf452
Onex101 <xeno@zarobi.co.za>
2018-08-23 17:48:12 +0200

    Fixed seg in draw_back

--- draw.c
+++ draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 11:50:52 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/08/23 17:30:51 by shillebr         ###   ########.fr       */
+/*   Updated: 2018/08/23 17:37:25 by xrhoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,69 +27,6 @@ void	draw_f_square(t_pnt *s, t_param *p, int c)
 		s->y++;
 		i++;
 	}
-}
-
-static void draw_c(t_param *p, int x, int y, int c)
-{
-	t_pnt pnt1;
-	t_pnt pnt2;
-
-	pnt1.x = x;
-	pnt1.y = y;
-	pnt2.x = p->player->pos->x;
-	pnt2.y = p->player->pos->y;
-	ft_putendl("draw_c test 1");
-	draw_line(&pnt1, &pnt2, p, c);
-	ft_putendl("draw_c test 2");
-}
-
-void draw_f_circle(t_pnt *s, t_param *p, int radius, int c)
-{
-	int x = radius - 1;
-	int y = 0;
-	int dx = 1;
-	int dy = 1;
-	int err = dx - (radius << 1);
-
-	ft_putendl("draw_circle test 1");
-	while (x >= y)
-	{
-		draw_c(p, s->x + x, s->y + y, c);
-		draw_c(p, s->x + y, s->y + x, c);
-		draw_c(p, s->x - y, s->y + x, c);
-		draw_c(p, s->x - x, s->y + y, c);
-		draw_c(p, s->x - x, s->y - y, c);
-		draw_c(p, s->x - y, s->y - x, c);
-		draw_c(p, s->x + y, s->y - x, c);
-		draw_c(p, s->x + x, s->y - y, c);
-		ft_putendl("draw_circle test 1_1");
-
-		if (err <= 0)
-		{
-			y++;
-			err += dy;
-			dy += 2;
-		}
-		
-		if (err > 0)
-		{
-			x--;
-			dx += 2;
-			err += dx - (radius << 1);
-		}
-	}
-	ft_putendl("draw_circle test 2");
-}
-
-void	draw_player(t_param *p)
-{
-	t_pnt s;
-
-	s.x = (p->player->pos->x);
-	s.y = (p->player->pos->y);
-	ft_putendl("draw_player test 1");
-	draw_f_circle(&s, p, TILE_SIZE / 8, P_COL);
-	ft_putendl("draw_player test 2");
 }
 
 void	draw_ray(t_pnt *pnt1, t_pnt *pnt2, t_param *p, int c)
@@ -121,7 +58,7 @@ void	draw_back(t_param *p)
 		col = rgb_to_hex(0, 0, c);
 		s1.x = 0;
 		s1.y = l;
-		s2.x = WIDTH;
+		s2.x = WIDTH - 1;
 		s2.y = l;
 		draw_line(&s1, &s2, p, col);
 		if (l % 10 == 0)
@@ -134,7 +71,7 @@ void	draw_back(t_param *p)
 		col = rgb_to_hex(c, 20, 20);
 		s1.x = 0;
 		s1.y = l;
-		s2.x = WIDTH;
+		s2.x = WIDTH - 1;
 		s2.y = l;
 		draw_line(&s1, &s2, p, col);
 		if (l % 10 == 0)
--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: xrhoda <xrhoda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 07:40:00 by xrhoda            #+#    #+#             */
-/*   Updated: 2018/08/23 15:08:01 by shillebr         ###   ########.fr       */
+/*   Updated: 2018/08/23 17:44:25 by xrhoda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+#include <string.h>
 #include "trig_tables.h"
 
 int		init_param(t_param *p, char *str)
@@ -24,47 +25,58 @@ int		init_param(t_param *p, char *str)
 	p->image = mlx_new_image(p->mlx, WIDTH, HEIGHT);
 	p->x_scale = WIDTH / p->map->max_x;
 	p->y_scale = HEIGHT / p->map->max_y;
-	if (!(p->player = ft_player_init(p)))
+	if(!(p->player = ft_player_init(p)))
 		return (0);
 	p->buf = (int *)mlx_get_data_addr(p->image, &bpp, &s_line, &end);
 	p->s_line = s_line;
+
 	return (1);
 }
 
 int		draw_to_screen(t_param *p)
 {
-	if (p->buf)
-		clear_image(p);
 	draw_back(p);
 	draw_map(p);
-	if (!(ft_rays(p, p->player)))
-		return (0);
+	if(!(ft_rays(p, p->player)))
+	 return (0);
 	mlx_put_image_to_window(p->mlx, p->win, p->image, 0, 0);
+	if (p->buf)
+		clear_image(p);
 	return (1);
 }
 
-int		main(int argc, char **argv)
+//int		main(int argc, char **argv)
+int		main(void)
 {
-	t_param		*param;
-	void		*mlx;
-	void		*win;
+	t_param	*param;
+	void *mlx;
+	void *win;
 
-	mlx = mlx_init();
+	mlx =  mlx_init();
+	if (!mlx)
+		return (-1);
 	win = mlx_new_window(mlx, WIDTH, HEIGHT, "Wolf3D");
-	if (argc == 2)
-	{
+	if (!win)
+		return (-1);
+	if (!win)
+		return (-1);
+	// if (argc == 2)
+	// {
 		if ((param = (t_param *)malloc(sizeof(t_param))))
 		{
 			param->mlx = mlx;
 			param->win = win;
-			if (!(init_param(param, argv[1])))
+			if (!(init_param(param, "maps/8")))
 				return (-1);
 			if (!param->mlx)
 				return (-1);
 			mlx_hook(win, 2, 0, key_press, param);
 			mlx_loop_hook(mlx, draw_to_screen, param);
-			mlx_loop(mlx);
+			if (mlx)
+				mlx_loop(mlx);
+			else
+				return (-1);
 		}
-	}
+	//}
 	return (0);
 }
